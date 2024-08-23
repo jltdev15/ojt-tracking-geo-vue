03d66034d84a92ac0d0386f7c82761954b85b922
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2024-08-23 22:14:49 +0800

    change base url

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  // baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://ojt-tracker-api.onrender.com/api/v1",
+  
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
