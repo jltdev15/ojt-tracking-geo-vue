7f3de43bbbdd104463433478f01bf71f03fa8d72
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2024-10-04 18:29:12 +0800

    add listing at homepage

--- src/components/Home/HTE.vue
+++ src/components/Home/HTE.vue
@@ -1,64 +1,25 @@
 <template>
   <div
     id="partner"
-    class="relative pt-20 md:h-screen md:overflow-hidden text-white bg-white"
+    class="relative pt-20 text-white bg-white md:h-screen md:overflow-hidden"
   >
     <div
-      class="container items-center relative z-10 flex justify-between h-full mx-auto text-center text-gray-800"
+      class="container relative z-10 flex items-center justify-between h-full mx-auto text-center text-gray-800"
     >
       <div class="w-full md:pt-32 md:pb-32">
-        <h1 class="font-bold text-xl md:text-5xl p-6 text-left text-primary">
+        <h1 class="p-6 text-xl font-bold text-left md:text-5xl text-primary">
           Partner's Host Training Establishments
         </h1>
-        <section class="grid md:grid-cols-4 gap-9 px-6">
-          <div
-            class="aspect-square flex gap-3 flex-col bg-gray-50 p-6 shadow-md rounded-lg"
-          >
-            <p class="text-justify text-3xl py-3 font-bold">VXI CLARK</p>
-            <p class="text-justify">
-              VXI Global Solutions is a leading customer experience and business process
-              outsourcing (BPO) company in the Philippines, renowned for its
-            </p>
-            <button class="mt-auto p-3 btn-primary btn">Apply Now</button>
-          </div>
-          <div
-            class="aspect-square flex gap-3 flex-col bg-gray-50 p-6 shadow-md rounded-lg"
-          >
-            <p class="text-justify text-3xl py-3 font-bold">Banco De Oro (BDO)</p>
-            <p class="text-justify">
-              BDO recognizes the importance of nurturing talent and investing in the next
-              generation of financial professionals. Through its internship program, BDO
-            </p>
-            <button class="mt-auto p-3 btn-primary btn">Apply Now</button>
-          </div>
-          <div
-            class="aspect-square flex gap-3 flex-col bg-gray-50 p-6 shadow-md rounded-lg"
-          >
-            <p class="text-left text-3xl py-3 font-bold">JPMorgan Chase & Co. Finance</p>
-            <p class="text-justify">
-              J.P. Morgan maintains a strong presence, offering career opportunities for
-              talented individuals seeking to make an impact in the finance industry.
-            </p>
-            <button class="mt-auto p-3 btn-primary btn">Apply Now</button>
-          </div>
-          <div
-            class="aspect-square flex gap-3 flex-col bg-gray-50 p-6 shadow-md rounded-lg"
-          >
-            <p class="text-left text-2xl py-3 font-bold">
-              International Business Machines Corporation
-            </p>
-            <p class="text-left">
-              IBM Philippines, a subsidiary of the global technology giant IBM, stands as
-              a beacon of innovation, expertise, and transformative solutions in th
-            </p>
-            <button class="mt-auto p-3 btn-primary btn">Apply Now</button>
-          </div>
+        <section class="bg">
+<InternshipItem :show="true"/>
         </section>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import InternshipItem from '../Student/InternshipItem.vue';
+</script>
 
 <style lang="scss" scoped></style>
