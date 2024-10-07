aa8896750377f53e02789435446ed325b594d5f0
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2024-10-07 15:39:27 +0800

    return to 15 meters

--- src/components/Hte/InternMonitoring.vue
+++ src/components/Hte/InternMonitoring.vue
@@ -101,7 +101,7 @@ const initMap = async () => {
       lat: authStore.hteLocationDefault.lat,
       lng: authStore.hteLocationDefault.lng,
     }, // Center of the geofence
-    radius: 500, // Geofence radius in meters
+    radius: 15, // Geofence radius in meters
   });
 };
 
